b0265373157ca648d0ad7f69e11d67c9f4da0455
Garrett Smith <garrett.smith@oregonstate.edu>
2016-09-12 17:40:46 -0700

    Static class for deserializing XML files to objects TFS work item "Read Xml to objects"

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Infrastructure/XmlSerialization.cs
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Infrastructure/XmlSerialization.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace SmarterBalanced.SampleItems.Dal.Infrastructure
 {
-    public class XmlSerialization
+    public static class XmlSerialization
     {
-       
+       public static T DeserializeXml<T>(FileInfo file)
+        {
+            T xml = default(T);
+
+            if (!file.Exists)
+            {
+                throw new Exception("File does not exist.");
+            }
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var reader = file.OpenRead())
+            {
+                xml = (T)serializer.Deserialize(reader);
+            }
+
+            return xml;
+        }
+
+
+        public static IEnumerable<T> DeserializeXmlFiles<T>(IEnumerable<FileInfo> files)
+        {
+            BlockingCollection<T> fileData = new BlockingCollection<T>();
+
+            Parallel.ForEach<FileInfo>(files, (file) =>
+            {
+                fileData.Add(DeserializeXml<T>(file));
+            });
+
+            return fileData;
+        }
     }
 }
