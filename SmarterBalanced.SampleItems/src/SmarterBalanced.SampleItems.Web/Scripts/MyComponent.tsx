3a16d9c19d26b735291c6e2cdae8c9a0ea5501de
schallee <schallee@CASS-DEV11>
2016-09-22 11:19:09 -0700

    Removed Bower, Node, webpack, moved to simple tsconfig

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/MyComponent.tsx
@@ -0,0 +1,14 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+/// <reference path="typings/globals/react-dom/index.d.ts" />
+/// <reference path="typings/globals/react/index.d.ts" />
+
+interface HelloProps { compiler: string; framework: string; }
+
+class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/wwwroot/js/app.js
+++
@@ -1,6 +0,0 @@
-function sayHello() {
-    var compiler = document.getElementById("compiler").value;
-    var framework = document.getElementById("framework").value;
-    return "blah blah blah blah " + compiler + " and " + framework;
-}
-//# sourceMappingURL=app.js.map
