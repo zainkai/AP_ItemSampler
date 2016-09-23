34b6bc2c47263dcd3603a2ae7923b383b31db9e0
gibsonri <gibsonri@CASS-DEV19>
2016-09-23 16:31:52 -0700

    More react experimenting-- it's not that suited for a basic web form

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/wwwroot/scripts/Components/GlobalAccommodationsComponent.js
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/wwwroot/scripts/Components/GlobalAccommodationsComponent.js
@@ -6,6 +6,14 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 var GlobalAccommodations;
 (function (GlobalAccommodations) {
     var Component = (function (_super) {
@@ -15,36 +23,58 @@ var GlobalAccommodations;
             this.state = this.props.initialState;
         }
         Component.prototype.render = function () {
+            var _this = this;
+            return (React.createElement("div", {className: "accommodations"}, 
+                React.createElement("h1", null, "All Accommodations"), 
+                this.renderFields(), 
+                React.createElement("button", {onClick: function () { return _this.report(); }}, "Submit")));
+        };
+        Component.prototype.renderFields = function () {
             var _a = this.state, presentation = _a.presentation, textToSpeech = _a.textToSpeech, masking = _a.masking, colorContrast = _a.colorContrast, highlighter = _a.highlighter, studentComments = _a.studentComments;
             return (React.createElement("div", null, 
+                React.createElement(ComboBox.Component, __assign({}, presentation)), 
+                React.createElement(ComboBox.Component, __assign({}, textToSpeech)), 
+                React.createElement(ComboBox.Component, __assign({}, masking)), 
+                React.createElement(ComboBox.Component, __assign({}, colorContrast)), 
                 React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Presentation:"), 
-                    comboBox(presentation)), 
-                React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Text-To-Speech:"), 
-                    comboBox(textToSpeech)), 
-                React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Masking:"), 
-                    comboBox(masking)), 
-                React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Color Contrast:"), 
-                    comboBox(colorContrast)), 
-                React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Highlighter:"), 
-                    React.createElement("input", {type: "checkbox", selected: highlighter})), 
-                React.createElement("div", null, 
-                    React.createElement("p", {style: { display: "inline-block" }}, "Student Comments:"), 
-                    comboBox(studentComments))));
+                    React.createElement("label", {htmlFor: "select-highlighter"}, "Highlighter:"), 
+                    React.createElement("input", {type: "checkbox", id: "select-highlighter", selected: highlighter})), 
+                React.createElement(ComboBox.Component, __assign({}, studentComments))));
+        };
+        Component.prototype.report = function () {
+            alert(JSON.stringify(this.state));
         };
         return Component;
     }(React.Component));
     GlobalAccommodations.Component = Component;
-    function comboBox(accommodation) {
-        var options = accommodation.options.map(function (name, idx) {
-            var isSelected = idx === accommodation.selectedIndex;
-            return React.createElement("option", {selected: isSelected}, name);
-        });
-        return (React.createElement("select", null, options));
-    }
 })(GlobalAccommodations || (GlobalAccommodations = {}));
+var ComboBox;
+(function (ComboBox) {
+    var Component = (function (_super) {
+        __extends(Component, _super);
+        function Component() {
+            _super.apply(this, arguments);
+            this.state = {
+                value: this.props.initialValue || this.props.options[0]
+            };
+        }
+        Component.prototype.render = function () {
+            var _this = this;
+            var options = this.props.options.map(function (name) { return React.createElement("option", {key: _this.props.id + "-" + name}, name); });
+            return (
+            // should event handlers be passed down?
+            // e => this.props.onChange((e.target as HTMLInputElement).value)
+            React.createElement("div", null, 
+                React.createElement("label", {htmlFor: this.props.id}, this.props.name), 
+                React.createElement("select", {id: this.props.id, onChange: function (e) { return _this.onChange(e); }}, options)));
+        };
+        Component.prototype.onChange = function (e) {
+            this.setState({
+                value: e.target.value
+            });
+        };
+        return Component;
+    }(React.Component));
+    ComboBox.Component = Component;
+})(ComboBox || (ComboBox = {}));
 //# sourceMappingURL=GlobalAccommodationsComponent.js.map
