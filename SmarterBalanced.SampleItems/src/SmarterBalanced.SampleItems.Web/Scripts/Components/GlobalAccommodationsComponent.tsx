b43d3e8f431bbedd72461ff862b39cc4f454a1d9
gibsonri <gibsonri@CASS-DEV19>
2016-09-23 12:05:17 -0700

    Begin experimenting with global accommodations

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/Components/GlobalAccommodationsComponent.tsx
@@ -0,0 +1,78 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+interface Accommodation {
+    name: string;
+    selectedIndex: number;
+    options: string[];
+}
+
+namespace GlobalAccommodations {
+    export interface State {
+        presentation: Accommodation;
+        textToSpeech: Accommodation;
+        masking: Accommodation;
+        colorContrast: Accommodation;
+        highlighter: boolean;
+        studentComments: Accommodation;
+    }
+
+    export interface Props {
+        initialState: State
+    }
+
+    export class Component extends React.Component<Props, State> {
+        state = this.props.initialState;
+
+        render() {
+            const { presentation, textToSpeech, masking, colorContrast, highlighter, studentComments } = this.state;
+            return (
+                <div>
+                    <div>
+                        <p style={{ display: "inline-block" }}>Presentation:</p>
+                        {comboBox(presentation)}
+                    </div>
+
+                    <div>
+                        <p style={{ display: "inline-block" }}>Text-To-Speech:</p>
+                        {comboBox(textToSpeech)}
+                    </div>
+
+                    <div>
+                        <p style={{ display: "inline-block" }}>Masking:</p>
+                        {comboBox(masking)}
+                    </div>
+
+                    <div>
+                        <p style={{ display: "inline-block" }}>Color Contrast:</p>
+                        {comboBox(colorContrast)}
+                    </div>
+
+                    <div>
+                        <p style={{ display: "inline-block" }}>Highlighter:</p>
+                        <input type="checkbox" selected={highlighter}></input>
+                    </div>
+
+                    <div>
+                        <p style={{ display: "inline-block" }}>Student Comments:</p>
+                        {comboBox(studentComments)}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    function comboBox(accommodation: Accommodation): JSX.Element {
+        const options: JSX.Element[] = accommodation.options.map(function (name, idx) {
+            const isSelected = idx === accommodation.selectedIndex;
+            return <option selected={isSelected}>{name}</option>;
+        });
+
+        return (
+            <select>
+                {options}
+            </select>
+        );
+    }
+}
---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/globalAccommodations.tsx
@@ -0,0 +1,25 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+/// <reference path="Components/GlobalAccommodationsComponent.tsx" />
+
+const example: Accommodation = {
+    name: "Example",
+    selectedIndex: 42,
+    options: ["foo", "bar", "baz"]
+}
+
+let initialState: GlobalAccommodations.State = {
+    presentation: example,
+    textToSpeech: { selectedIndex: 0, options: [], name: "" },
+    masking: { selectedIndex: 0, options: [], name: "" },
+    colorContrast: { selectedIndex: 0, options: [], name: "" },
+    highlighter: false,
+    studentComments: { selectedIndex: 0, options: [], name: "" }
+}
+
+ReactDOM.render(
+    <GlobalAccommodations.Component initialState={initialState} />,
+    document.getElementById("container") as HTMLElement
+);
