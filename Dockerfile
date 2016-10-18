2c62bfb1fddb532f1d4e49d7f05dbbaa77b55ad8
alexlepinski <alex.lepinski@oregonstate.edu>
2016-10-18 16:11:26 -0700

    working on aws publish

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM microsoft/aspnetcore-build
+ARG source=./SmarterBalanced.SampleItems
+WORKDIR /app
+COPY $source .
+
 RUN apt-get install -y \
     python
 
 RUN curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" && python get-pip.py &&  pip install awscli
+
+ENTRYPOINT ["sh", "./build.sh"]
--- SmarterBalanced.SampleItems/build.sh
+++ SmarterBalanced.SampleItems/build.sh
@@ -1,3 +1,19 @@
 #!bin/bash
 set -e
-cp /content_mnt -r /content
+dotnet restore
+rm -rf /app/publish/
+dotnet publish src/SmarterBalanced.SampleItems.Web/project.json -c release -o /app/publish/web
+cp AccessibilityAccommodationConfigurations/AccessibilityConfig.xml /app/publish/web/configuration
+
+#get ec2 content
+mkdir -p /app/publish/web/content
+aws s3 cp s3://cass-sb-itemviewerservice-content/content.zip content.zip
+unzip content.zip -d /app/publish/web/content/
+rm content.zip
+
+#Docker build app
+cd /app/publish/web/
+aws ecr get-login --region us-west-2
+docker build -t sampleitemsapp .
+docker tag sampleitemsapp:latest 047189625337.dkr.ecr.us-west-2.amazonaws.com/sampleitemsapp:latest
+docker push 047189625337.dkr.ecr.us-west-2.amazonaws.com/sampleitemsapp:latest
