a9c8d5596fe6a0b3bdabd959ed39b090991ac2f3
Garrett Smith <garrett.smith@oregonstate.edu>
2016-10-24 16:55:13 -0700

    Create docker image for all branches

--- CodeDeploy/dockerDeploy.sh
+++ CodeDeploy/dockerDeploy.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -ev
 
-if [$TRAVIS_BRANCH == 'dev'] || [$TRAVIS_BRANCH == 'stage'] || [$TRAVIS_BRANCH == 'master']; then
-    docker --version
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region us-west-2)
-    dotnet restore /home/travis/build/osu-cass/SampleItemsWebsite/SmarterBalanced.SampleItems/
-    dotnet publish /home/travis/build/osu-cass/SampleItemsWebsite/SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/project.json
-    cd /home/travis/build/osu-cass/SampleItemsWebsite/SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/bin/Debug/netcoreapp1.0/publish
-    docker build -t sampleitemswebsite .
-    docker tag sampleitemswebsite:latest 047189625337.dkr.ecr.us-west-2.amazonaws.com/sampleitemswebsite:$TRAVIS_BRANCH
-    docker push 047189625337.dkr.ecr.us-west-2.amazonaws.com/sampleitemswebsite:$TRAVIS_BRANCH
-fi
-
 if [$TRAVS_BRANCH == 'dev'] then;
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_dev
 fi
