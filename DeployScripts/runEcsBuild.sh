9b0a1a1232b4175981709fbcc06a14404256b1e8
Garrett Smith <garrett.smith@oregonstate.edu>
2016-10-25 11:20:41 -0700

    Give ECS run script better name

--- DeployScripts/runEcsBuild.sh
+++ DeployScripts/runEcsBuild.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -ev
 
-if [$TRAVS_BRANCH == 'dev'] then;
+if [ "$TRAVS_BRANCH" == "dev" ]; then
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_dev
 fi
 
-if [$TRAVS_BRANCH == 'stage'] then;
+if  [ "$TRAVS_BRANCH" == "stage" ]; then
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_stage
 fi
 
-if [$TRAVS_BRANCH == 'master'] then;
+if [ "$TRAVS_BRANCH" == "master" ]; then
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_master
 fi
 
-if [$TRAVS_BRANCH == 'feature_dockeraws'] then;
+if [ "$TRAVS_BRANCH" == "feature_dockeraws" ]; then
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_dev
 fi
