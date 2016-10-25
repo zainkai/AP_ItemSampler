30cf3d1e88306d096341cff0d8c9621abe7796e3
Garrett Smith <garrett.smith@oregonstate.edu>
2016-10-25 10:41:27 -0700

    Test running ecs tasks from Travis

--- CodeDeploy/dockerDeploy.sh
+++ CodeDeploy/dockerDeploy.sh
@@ -12,3 +12,7 @@ fi
 if [$TRAVS_BRANCH == 'master'] then;
     aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_master
 fi
+
+if [$TRAVS_BRANCH == 'feature_dockeraws'] then;
+    aws ecs run-task --region us-west-2 --cluster SampleItemsBuild --count 1 --task-definition SampleItemsBuild_dev
+fi
