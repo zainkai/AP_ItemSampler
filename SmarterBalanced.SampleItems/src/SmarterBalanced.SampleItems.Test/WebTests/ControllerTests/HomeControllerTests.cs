cba2c7146f233a58b95f9dc44a83bc3b015d5a5d
Evan Schallerer <schallee@oregonstate.edu>
2017-01-19 12:05:44 -0800

    templated home controller tests

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Test/WebTests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using Moq;
+using SmarterBalanced.SampleItems.Web.Controllers;
+using SmarterBalanced.SampleItems.Dal.Configurations.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmarterBalanced.SampleItems.Test.WebTests.ControllerTests
+{
+    public class HomeControllerTests
+    {
+
+        HomeController HomeController;
+
+        public HomeControllerTests()
+        {
+            var appSettings = new AppSettings()
+            {
+                SettingsConfig = new SettingsConfig()
+            };
+
+            var loggerFactory = new Mock<ILoggerFactory>();
+            var logger = new Mock<ILogger>();
+            loggerFactory.Setup(lf => lf.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+
+            HomeController = new HomeController(appSettings, loggerFactory.Object);
+        }
+
+        [Fact (Skip ="TODO: Test each of the status codes")]
+        public void StatusCodeTest()
+        {
+
+        }
+
+
+    }
+}
