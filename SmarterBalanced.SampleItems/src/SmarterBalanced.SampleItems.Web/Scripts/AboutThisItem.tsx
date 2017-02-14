2a57df504c3468c740ba853074beb1225187cb5f
Rikki Gibson <rikkigibson@gmail.com>
2017-02-14 15:16:24 -0800

    Switch card styling in about item modal

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/AboutThisItem.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/AboutThisItem.tsx
@@ -115,36 +115,32 @@ class AboutThisItemDetailComponent extends React.Component<ItemCardViewModel, {}
     render() {
         const { bankKey, itemKey } = this.props;
         return (
-            <div className={`card card-block card-static ${this.props.subjectCode.toLowerCase()}`}
-                tabIndex={0}>
-                <div className="card-contents">
-                    <h4 className="card-title">{this.props.title}</h4>
-                    <p className="card-text subject">
-                        <span className="card-text-label">Subject:</span>
-                        <span className="card-text-value"> {this.props.subjectLabel}</span>
-                    </p>
-                    <p className="card-text grade">
-                        <span className="card-text-label">Grade:</span>
-                        <span className="card-text-value"> {this.props.gradeLabel}</span>
-                    </p>
-                    <p className="card-text claim">
-                        <span className="card-text-label">Claim:</span>
-                        <span className="card-text-value"> {this.props.claimLabel}</span>
-                    </p>
-                    <p className="card-text target">
-                        <span className="card-text-label">Target:</span>
-                        <span className="card-text-value"> {this.props.target}</span>
-                    </p>
-                    <p className="card-text interaction-type">
-                        <span className="card-text-label">Item Type:</span>
-                        <span className="card-text-value"> {this.props.interactionTypeLabel}</span>
-                    </p>
-                    <p className="card-text item-id">
-                        <span className="card-text-label">Item Id:</span>
-                        <span className="card-text-value"> {this.props.itemKey}</span>
-                    </p>
-                </div>
-            </div>
+            <div>
+                <p className="card-text subject" tabIndex={0}>
+                    <span className="card-text-label">Subject:</span>
+                    <span className="card-text-value"> {this.props.subjectLabel}</span>
+                </p>
+                <p className="card-text grade" tabIndex={0}>
+                    <span className="card-text-label">Grade:</span>
+                    <span className="card-text-value"> {this.props.gradeLabel}</span>
+                </p>
+                <p className="card-text claim" tabIndex={0}>
+                    <span className="card-text-label">Claim:</span>
+                    <span className="card-text-value"> {this.props.claimLabel}</span>
+                </p>
+                <p className="card-text target" tabIndex={0}>
+                    <span className="card-text-label">Target:</span>
+                    <span className="card-text-value"> {this.props.target}</span>
+                </p>
+                <p className="card-text interaction-type" tabIndex={0}>
+                    <span className="card-text-label">Item Type:</span>
+                    <span className="card-text-value"> {this.props.interactionTypeLabel}</span>
+                </p>
+                <p className="card-text item-id" tabIndex={0}>
+                    <span className="card-text-label">Item Id:</span>
+                    <span className="card-text-value"> {this.props.itemKey}</span>
+                </p>
+        </div>
         );
     }
 }
