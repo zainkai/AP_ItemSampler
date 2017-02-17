bf0daee40b217b12e2bc490f3f10b6ce0c81b946
Evan Schallerer <schallee@oregonstate.edu>
2017-02-16 17:40:03 -0800

    adding equality comparisons for StandardIdentifier for CCSS and Target

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Xml/Models/StandardIdentifier.cs
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Xml/Models/StandardIdentifier.cs
@@ -7,6 +7,7 @@ namespace SmarterBalanced.SampleItems.Dal.Xml.Models
 {
     public class StandardIdentifier
     {
+        public string SubjectCode { get; }
         public string Claim { get; }
         public string ContentDomain { get; }
         public string ContentCategory { get; }
@@ -15,7 +16,9 @@ namespace SmarterBalanced.SampleItems.Dal.Xml.Models
         public string Emphasis { get; }
         public string CommonCoreStandard { get; }
 
-        public StandardIdentifier(string claim,
+        public StandardIdentifier(
+            string subjectCode,
+            string claim,
             string target,
             string contentDomain = null,
             string contentCategory = null,
@@ -23,6 +26,7 @@ namespace SmarterBalanced.SampleItems.Dal.Xml.Models
             string emphasis = null,
             string commonCoreStandard = null)
         {
+            SubjectCode = subjectCode;
             Claim = claim;
             ContentDomain = contentDomain;
             ContentCategory = contentCategory;
@@ -31,5 +35,82 @@ namespace SmarterBalanced.SampleItems.Dal.Xml.Models
             Emphasis = emphasis;
             CommonCoreStandard = commonCoreStandard;
         }
+
     }
+
+    public class StandardIdentifierTargetComparer : IEqualityComparer<StandardIdentifier>
+    {
+        public static StandardIdentifierTargetComparer Instance { get; } = new StandardIdentifierTargetComparer();
+        private StandardIdentifierTargetComparer() { }
+
+        public bool Equals(StandardIdentifier x, StandardIdentifier y)
+        {
+            if(x.SubjectCode != y.SubjectCode)
+            {
+                return false;
+            }
+
+            if(x.SubjectCode == "MA") // TODO: Verify MA or MATH?
+            {
+                return x.Target == y.Target &&
+                    x.Claim == y.Claim &&
+                    x.ContentDomain == y.ContentDomain;
+            }
+            else if (x.SubjectCode == "ELA")
+            {
+                return x.Target == y.Target &&
+                    x.Claim == y.Claim;
+            }
+
+            return false;
+        }
+
+        public int GetHashCode(StandardIdentifier obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+    /// MATH:
+    ///     CommonCoreStandard must match Claim, ContentDomain, Target, 
+    ///         Emphasis, and CommonCoreStandard
+    /// ELA:
+    ///     Target must match Claim and Target
+    ///     CommonCoreStandard must match Claim, Target, and CommonCoreStandard
+    public class StandardIdentifierCcssComparer : IEqualityComparer<StandardIdentifier>
+    {
+        public static StandardIdentifierCcssComparer Instance { get; } = new StandardIdentifierCcssComparer();
+        private StandardIdentifierCcssComparer() { }
+
+        public bool Equals(StandardIdentifier x, StandardIdentifier y)
+        {
+            if (x.SubjectCode != y.SubjectCode)
+            {
+                return false;
+            }
+
+            if (x.SubjectCode == "MATH")
+            {
+                return x.Claim == y.Claim &&
+                    x.ContentDomain == y.ContentDomain &&
+                    x.Target == y.Target &&
+                    x.Emphasis == y.Emphasis &&
+                    x.CommonCoreStandard == y.CommonCoreStandard;
+            }
+            else if (x.SubjectCode == "ELA")
+            {
+                return x.Claim == y.Claim &&
+                    x.Target == y.Target &&
+                    x.CommonCoreStandard == y.CommonCoreStandard;
+            }
+
+            return false;
+        }
+
+        public int GetHashCode(StandardIdentifier obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+
 }
