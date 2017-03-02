876a8561560f3c7d5ccefea10146b528cb2c00d0
alexlepinski <alex.lepinski@oregonstate.edu>
2017-03-02 09:50:03 -0800

    resolved merge conflict with peformance items

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Core/Repos/ItemViewRepo.cs
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Core/Repos/ItemViewRepo.cs
@@ -66,7 +66,7 @@ namespace SmarterBalanced.SampleItems.Core.Repos
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        private List<SampleItem> GetAssociatedStimulus(SampleItem item)
+        private List<SampleItem> GetAssociatedPerformanceItems(SampleItem item)
         {
             var associatedStimulus = item.AssociatedStimulus;
             List<SampleItem> associatedStimulusDigests = context.SampleItems
