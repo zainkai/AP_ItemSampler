bec466edaa56f6ff862d659b85e6905976a9dbd6
alexlepinski <alex.lepinski@oregonstate.edu>
2017-03-16 09:06:28 -0700

    added null coalascing for calc

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Translations/ItemDigestTranslation.cs
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Dal/Translations/ItemDigestTranslation.cs
@@ -92,7 +92,7 @@ namespace SmarterBalanced.SampleItems.Dal.Translations
                 SufficentEvidenceOfClaim = itemMetadata.Metadata.SufficientEvidenceOfClaim,
                 AssociatedStimulus = itemMetadata.Metadata.AssociatedStimulus,
                 AslSupported = (itemMetadata.Metadata.AccessibilityTagsASLLanguage ?? "Y") == "Y",
-                AllowCalculator = itemMetadata.Metadata.AllowCalculator == "Y",
+                AllowCalculator = (itemMetadata.Metadata.AllowCalculator ?? "Y") == "Y",
                 DepthOfKnowledge = itemMetadata.Metadata.DepthOfKnowledge,
                 Contents = itemContents.Item.Contents,
                 InteractionTypeCode = interactionCode,
