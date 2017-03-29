27ef0816a609f83c89121b916de87d7f235694bd
struthj <struthj@oregonstate.edu>
2017-03-29 16:57:00 -0700

    started SampleItemTranslation Tests

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Test/DalTests/TranslationsTests/SampleItemTranslationTests.cs
@@ -0,0 +1,46 @@
+﻿using SmarterBalanced.SampleItems.Dal.Configurations.Models;
+using SmarterBalanced.SampleItems.Dal.Providers.Models;
+using SmarterBalanced.SampleItems.Dal.Translations;
+using SmarterBalanced.SampleItems.Dal.Xml.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmarterBalanced.SampleItems.Test.DalTests.TranslationsTests
+{
+    public class SampleItemTranslationTests
+    {
+        [Fact(Skip ="ToDo")]
+        public void TestToSampleItems()
+        {
+
+        }
+
+        [Fact(Skip = "ToDo")]
+        public void TestToSampleItem()
+        {
+
+        }
+
+        [Fact(Skip = "ToDo")]
+        public void TestGroupItemResources()
+        {
+
+        }
+
+        [Fact(Skip = "ToDo")]
+        public void TestGetRubrics()
+        {
+
+        }
+
+        [Fact(Skip = "ToDo")]
+        public void TestToRubric()
+        {
+
+        }
+    }
+}
