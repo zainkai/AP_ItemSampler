dfa73d7d8375258470504e072b73cf389d04354b
Garrett Smith <garrett.smith@oregonstate.edu>
2017-04-14 13:53:02 -0700

    Moved click/keyhandlers to the header
    The screen reader is clicking on the header when users try to open the item
    card link with the enter key. I added handers to the header so it has the
    expected behavior.

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/ItemCard.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/ItemCard.tsx
@@ -33,10 +33,12 @@ class ItemCard extends React.Component<ItemCardViewModel, {}> {
                 onClick={e => itemPageLink(bankKey, itemKey)}
                 onKeyUp={e => this.handleKeyPress(bankKey, itemKey, e)}
                 tabIndex={0}>
-                <div className="card-contents"
-                    onClick={e => itemPageLink(bankKey, itemKey)}
-                    onKeyUp={e => this.handleKeyPress(bankKey, itemKey, e)}>
-                    <h4 className="card-title">{this.props.title}</h4>
+                <div className="card-contents">
+                    <h4 className="card-title"
+                        onClick={e => itemPageLink(bankKey, itemKey)}
+                        onKeyUp={e => this.handleKeyPress(bankKey, itemKey, e)}>
+                        {this.props.title}
+                    </h4>
                     <p className="card-text subject">
                         <span className="card-text-label">Subject:</span>
                         <span className="card-text-value"> {this.props.subjectLabel}</span>
