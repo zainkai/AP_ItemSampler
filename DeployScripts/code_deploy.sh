c1c3302c397ddb0cf377f86841b8aa8c129633ca
alexlepinski <alex.lepinski@oregonstate.edu>
2017-05-08 15:00:34 -0700

    updated travis

--- DeployScripts/code_deploy.sh
+++ DeployScripts/code_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-cd /home/travis/build/osu-cass/SampleItemsWebsite/publish/
+cd /publish/
 docker build -t sampleitemscode .
 docker tag sampleitemscode:latest osucass/sampleitemscode:$BRANCH
 docker push osucass/sampleitemscode:$BRANCH
