dd7f96f497e98d540d86bed3ee7ca2cd2e8eb3c2
Rutger Farry <rutgerfarry@gmail.com>
2017-08-14 15:34:34 -0700

    Add dummy json and blank ItemsSearchParams test

---
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Tests/ItemsSearchParamsTests.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import * as stubbedModel from './stubbedResponse';
+import { ISPComponent } from '../Scripts/ItemsSearchParams';
+
+describe('ItemsSearchParams', () => {
+    test('renders', () => {
+        expect('this needs to be done').toBeDefined();
+    });
+});
