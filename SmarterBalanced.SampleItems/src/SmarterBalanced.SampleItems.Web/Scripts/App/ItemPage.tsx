56c07a3473c3d8f0a3df0c0637add9e447086192
Kevin Turkington <turkingtonkevin@gmail.com>
2017-09-07 12:50:30 -0700

    refactoring modal links in itempage

--- SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/App/ItemPage.tsx
+++ SmarterBalanced.SampleItems/src/SmarterBalanced.SampleItems.Web/Scripts/App/ItemPage.tsx
@@ -170,59 +170,78 @@ namespace ItemPage {
             );
         }
 
-        render() {
+        renderModalLinks(): JSX.Element {
+            const abtText = <span>About <span className="item-nav-long-label">This Item</span></span>;
+            const moreText = <span>More <span className="item-nav-long-label">Like This</span></span>;
+
+            return (
+                <div className="item-nav" role="toolbar" aria-label="Toolbar with button groups">
+                    <div className="item-nav-left-group" role="group" aria-label="First group">
+
+                        <a className="item-nav-btn" data-toggle="modal" data-target="#about-item-modal-container"
+                            onKeyUp={e => this.openAboutItemModal(e)} role="button" tabIndex={0}>
+                            <span className="glyphicon glyphicon-info-sign glyphicon-pad" aria-hidden="true" />
+                            {abtText}
+                        </a>
+
+                        <a className="item-nav-btn" data-toggle="modal" data-target="#more-like-this-modal-container"
+                            onKeyUp={e => this.openMoreLikeThisModal(e)} role="button" tabIndex={0}>
+                            <span className="glyphicon glyphicon-th-large glyphicon-pad" aria-hidden="true" />
+                            {moreText}
+                        </a>
+
+                        <a className="item-nav-btn" data-toggle="modal" data-target="#share-modal-container"
+                            onKeyUp={e => this.openShareModal(e)} role="button" tabIndex={0}>
+                            <span className="glyphicon glyphicon-share-alt glyphicon-pad" aria-hidden="true" />
+                            Share
+                                </a>
+
+                        {this.renderPerformanceItemModalBtn(this.props.isPerformanceItem)}
+
+                        <Braille.BrailleLink
+                            currentSelectionCode={Accessibility.getBrailleAccommodation(this.props.accResourceGroups)}
+                            brailleItemCodes={this.props.brailleItemCodes}
+                            braillePassageCodes={this.props.braillePassageCodes}
+                            bankKey={this.props.currentItem.bankKey}
+                            itemKey={this.props.currentItem.itemKey} />
+                    </div>
+                    {this.renderAccessabilityBtn()}
+                </div>
+                
+                );
+        }
+
+        renderAccessabilityBtn(): JSX.Element {
+
+            return (
+                <div className="item-nav-right-group" role="group" aria-label="Second group">
+                    <button className="accessibility-btn btn btn-primary" data-toggle="modal"
+                        data-target="#accessibility-modal-container"
+                        onClick={e => ga("send", "event", "button", "OpenAccessibility")}
+                        onKeyUp={e => this.openAccessibilityModal(e)} tabIndex={0}>
+                        <span className="glyphicon glyphicon-collapse-down" aria-hidden="true"></span>
+                        Accessibility
+                            </button>
+                </div>
+                );
+        }
+
+        renderIFrame(): JSX.Element {
             let isaap = toiSAAP(this.props.accResourceGroups);
             const itemNames = (Accessibility.isBrailleEnabled(this.props.accResourceGroups)) ? this.props.brailleItemNames : this.props.itemNames;
             let scrollTo: string = Accessibility.isStreamlinedEnabled(this.props.accResourceGroups) ? "" : ("&scrollToId=").concat(this.props.currentItem.itemName);
             let ivsUrl: string = this.props.itemViewerServiceUrl.concat("/items?ids=", itemNames, "&isaap=", isaap, scrollTo);
 
-            const abtText = <span>About <span className="item-nav-long-label">This Item</span></span>;
-            const moreText = <span>More <span className="item-nav-long-label">Like This</span></span>;
+            return (<ItemFrame url={ivsUrl} />);
+        }
+
+        render() {
+            let isaap = toiSAAP(this.props.accResourceGroups);
+
             return (
                 <div>
-                    <div className="item-nav" role="toolbar" aria-label="Toolbar with button groups">
-                        <div className="item-nav-left-group" role="group" aria-label="First group">
-
-                            <a className="item-nav-btn" data-toggle="modal" data-target="#about-item-modal-container"
-                                onKeyUp={e => this.openAboutItemModal(e)} role="button" tabIndex={0}>
-                                <span className="glyphicon glyphicon-info-sign glyphicon-pad" aria-hidden="true" />
-                                {abtText}
-                            </a>
-
-                            <a className="item-nav-btn" data-toggle="modal" data-target="#more-like-this-modal-container"
-                                onKeyUp={e => this.openMoreLikeThisModal(e)} role="button" tabIndex={0}>
-                                <span className="glyphicon glyphicon-th-large glyphicon-pad" aria-hidden="true" />
-                                {moreText}
-                            </a>
-
-                            <a className="item-nav-btn" data-toggle="modal" data-target="#share-modal-container"
-                                onKeyUp={e => this.openShareModal(e)} role="button" tabIndex={0}>
-                                <span className="glyphicon glyphicon-share-alt glyphicon-pad" aria-hidden="true" />
-                                Share
-                            </a>
-
-                            {this.renderPerformanceItemModalBtn(this.props.isPerformanceItem)}
-
-                            <Braille.BrailleLink
-                                currentSelectionCode={Accessibility.getBrailleAccommodation(this.props.accResourceGroups)}
-                                brailleItemCodes={this.props.brailleItemCodes}
-                                braillePassageCodes={this.props.braillePassageCodes}
-                                bankKey={this.props.currentItem.bankKey}
-                                itemKey={this.props.currentItem.itemKey} />
-
-                        </div>
-
-                        <div className="item-nav-right-group" role="group" aria-label="Second group">
-                            <button className="accessibility-btn btn btn-primary" data-toggle="modal"
-                                data-target="#accessibility-modal-container"
-                                onClick={e =>ga("send", "event", "button", "OpenAccessibility")}
-                                onKeyUp={e => this.openAccessibilityModal(e)} tabIndex={0}>
-                                <span className="glyphicon glyphicon-collapse-down" aria-hidden="true"></span>
-                                Accessibility
-                            </button>
-                        </div>
-                    </div>
-                    <ItemFrame url={ivsUrl} />
+                    {this.renderModalLinks()}
+                    {this.renderIFrame()}
                     <AboutThisItem.ATIComponent {...this.props.aboutThisItemVM} />
                     <AccessibilityModal.ItemAccessibilityModal
                         accResourceGroups={this.props.accResourceGroups}
